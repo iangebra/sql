9ab3995e19b55547cb5893ca9587a47e3cff8fad
Ivan <132353260+iangebra@users.noreply.github.com>
2023-05-08 14:41:50 +0200

    Add files via upload

---
+++ frizerski.sql
@@ -0,0 +1,38 @@
+use master;
+drop database if exists frizerski;
+go
+create database frizerski;
+go
+use frizerski;
+
+create table djelatnica(
+	ime varchar(50),
+	prezime varchar(50),
+	sifradjelatnice varchar(50),
+	adresa varchar(50)
+	
+);
+
+create table korisnik(
+	ime varchar(50),
+	prezime varchar(50),
+	spol varchar(50),
+	mobitel varchar(100),
+	mail varchar(50),
+	sifrakorisnika varchar(50),
+	adresa varchar(50),
+	vrstausluge varchar,
+);
+
+create table usluga(
+	vrstausluge varchar(50),
+	cijena decimal, 
+	sifradjelatnice varchar(50),
+	trajanje int,
+);
+
+create table termin
+(
+    datumtermina date,
+    sifrakorisnika varchar(10),
+	);
