d5ac83204ac415deab5de2d766d6c0f8e56662b2
Ivan <ange987@gmail.com>
2023-05-17 15:16:51 +0200

    Update muzej.sql

--- muzej.sql
+++ muzej.sql
@@ -6,28 +6,46 @@ go
 use muzej;
 
 create table kustos(
+	sifra int not null primary key identity(1,1),
 	ime varchar(50),
 	prezime varchar(50),
-	sifrakustosa varchar(50),
-	adresa varchar(50),
 	mobitel varchar(100),
-	mail varchar(50),
-	
-);
-
-create table djelo(
-	imeAutora varchar(50),
-	prezimeAutora varchar(50),
-	izlozba varchar(50),
-	godinaIzrade varchar(50),
 	
 );
 
 create table izlozba(
+	sifra int not null primary key identity(1,1),
 	naslov varchar(50),
-	sifrakustosa varchar(50),
 	datum date,
-	vrijeme time,
 	trajanje int,
 	sponzor varchar(50)
 );
+
+create table djelo(
+	sifra int not null primary key identity(1,1),
+	naslov varchar(50),
+	kustos int,
+	izlozba int,
+	godinaIzrade varchar(50)
+		
+);
+
+
+alter table djelo add foreign key (kustos) references kustos (sifra);
+alter table djelo add foreign key (izlozba) references izlozba (sifra);
+	
+
+insert into kustos (ime,prezime,mobitel)
+values ('ana','anic','09123456'),
+('lana','lanic','092123356'),
+('mara','maric','0983456');
+
+insert into izlozba (naslov,datum,trajanje,sponzor)
+values ('izlozba 1','2020-06-01',180,'coca cola'),
+('izlozba 2','2021-08-01',50,'nike'),
+('izlozba 3','2022-04-04',120,'HEP');
+
+insert into djelo (naslov,kustos,izlozba,godinaIzrade)
+values ('djelo1',1,1,1999),
+('djelo2',2,1,2022),
+('djelo3',3,2,2015);
